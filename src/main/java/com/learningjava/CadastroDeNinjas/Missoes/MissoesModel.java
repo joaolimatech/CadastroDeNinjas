2b6d604407927bd8866414bda2e19d6d53a852ec
Joao Lima <joaolimatech@gmail.com>
2026-08-08 00:36:47 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/com/learningjava/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/learningjava/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,19 @@ package com.learningjava.CadastroDeNinjas.Missoes;
 
 import com.learningjava.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class MissoesModel {
 
     @Id
