c29065f6898023b321d25ecc67310ba41c15f438
Joao Lima <joaolimatech@gmail.com>
2026-08-11 20:18:00 -0300

    feat: Atualiza controller Ninjas com mock de dados

--- src/main/java/com/learningjava/CadastroDeNinjas/Missoes/controller/MissoesController.java
+++ src/main/java/com/learningjava/CadastroDeNinjas/Missoes/controller/MissoesController.java
@@ -1,4 +1,7 @@
 package com.learningjava.CadastroDeNinjas.Missoes.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MissoesController {
 }
--- src/main/java/com/learningjava/CadastroDeNinjas/Ninjas/controller/NinjaController.java
+++ src/main/java/com/learningjava/CadastroDeNinjas/Ninjas/controller/NinjaController.java
@@ -1,8 +1,6 @@
 package com.learningjava.CadastroDeNinjas.Ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,36 @@ public class NinjaController {
     public String boasVindas(){
         return "Bem vindo a aplicacao";
     }
+
+    //Add ninja
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "ninja criado";
+    }
+
+    // procurar ninja
+    @GetMapping("/todos")
+    public String mostrarTOdosNinjas(){
+        return "todos ninjas";
+    }
+
+    //mostrat todos ninjas (READ)
+    @GetMapping("/todosID")
+    public String mostrarNinjasID(){
+        return "mostrar por ID";
+    }
+
+    //alterar dados do ninja
+    @PutMapping("/alterarID")
+    public String alterarNinjaId(){
+        return "Alterar ninja";
+    }
+
+    @DeleteMapping("/deleteID")
+    public String deletarNinja(){
+        return "deletar ninjas";
+    }
+
+    //deletar ninja
 
 }
